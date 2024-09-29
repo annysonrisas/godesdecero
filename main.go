f2d052174363a7073492956dc59382492479ab5d
ad311636 <airene_delacruz@bhd.com.do>
2024-09-29 18:32:26 -0400

    subir funciones

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github/annysonrisas/godesdecero/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }

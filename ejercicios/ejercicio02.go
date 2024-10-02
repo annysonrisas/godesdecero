6a153fd22b94313dc651a37c2dd291a05a8d7987
ad311636 <airene_delacruz@bhd.com.do>
2024-10-02 11:36:42 -0400

    manejo de archivos

--- ejercicios/ejercicio02.go
+++ ejercicios/ejercicio02.go
@@ -9,10 +9,12 @@ import (
 
 var numero1 int
 var err error
+var texto string
 
-func TabladeMultiplicar() {
+func TabladeMultiplicar() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	exit := 1
+
 	for exit == 1 {
 		fmt.Println("Ingrese un numero: ")
 		if scanner.Scan() {
@@ -21,10 +23,12 @@ func TabladeMultiplicar() {
 				fmt.Println("numero ingresado incorrecto.")
 			} else {
 				for i := 1; i <= 10; i++ {
-					fmt.Println(numero1, " X ", i, " =", numero1*i)
+					texto += fmt.Sprintln(numero1, " X ", i, " =", numero1*i)
 				}
 				exit = 5
 			}
 		}
 	}
+
+	return texto
 }
--- main.go
+++ main.go
@@ -1,29 +1,33 @@
 package main
 
-import "github/annysonrisas/godesdecero/ejercicios"
+import "github/annysonrisas/godesdecero/files"
 
 func main() {
+
 	/*	estado, texto := variables.ConviertoaTexto(1588)
-		fmt.Println(estado)
-		fmt.Println(texto)*/
-	/*if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-		fmt.Println("Esto no es Windows, es", os)
-	} else {
-		fmt.Println("Esto es", os)
-	}
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es Linux")
-	case "darwin":
-		fmt.Println("Esto es Darwin")
-	default:
-		fmt.Printf("%s \n", os)
+			fmt.Println(estado)
+			fmt.Println(texto)
+		if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+			fmt.Println("Esto no es Windows, es", os)
+		} else {
+			fmt.Println("Esto es", os)
+		}
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("Esto es Linux")
+		case "darwin":
+			fmt.Println("Esto es Darwin")
+		default:
+			fmt.Printf("%s \n", os)
 
-	numero, texto := ejercicios.ConviertoaNumerico("40")
-	fmt.Println(numero)
-	fmt.Println(texto)
-		}*/
+		numero, texto := ejercicios.ConviertoaNumerico("40")
+		fmt.Println(numero)
+		fmt.Println(texto)
+			}
 
-	ejercicios.TabladeMultiplicar()
+		fmt.Println(ejercicios.TabladeMultiplicar())
+	*/
+	//files.GrabaTabla()
+	files.LeoArchivo()
 
 }

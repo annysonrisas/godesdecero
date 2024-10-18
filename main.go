05d6bd7b2e8c66c700c13d064ed80c8bc9600945
ad311636 <airene_delacruz@bhd.com.do>
2024-10-18 16:24:57 -0400

    subir ultimas practicas

--- main.go
+++ main.go
@@ -1,52 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"github/annysonrisas/godesdecero/gorutines"
+	"github/annysonrisas/godesdecero/middleware"
 )
 
 func main() {
 
 	/*	estado, texto := variables.ConviertoaTexto(1588)
-					fmt.Println(estado)
-					fmt.Println(texto)
-				if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-					fmt.Println("Esto no es Windows, es", os)
-				} else {
-					fmt.Println("Esto es", os)
-				}
-				switch os := runtime.GOOS; os {
-				case "linux":
-					fmt.Println("Esto es Linux")
-				case "darwin":
-					fmt.Println("Esto es Darwin")
-				default:
-					fmt.Printf("%s \n", os)
-
-				numero, texto := ejercicios.ConviertoaNumerico("40")
-				fmt.Println(numero)
-				fmt.Println(texto)
+						fmt.Println(estado)
+						fmt.Println(texto)
+					if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+						fmt.Println("Esto no es Windows, es", os)
+					} else {
+						fmt.Println("Esto es", os)
 					}
+					switch os := runtime.GOOS; os {
+					case "linux":
+						fmt.Println("Esto es Linux")
+					case "darwin":
+						fmt.Println("Esto es Darwin")
+					default:
+						fmt.Printf("%s \n", os)
 
-				fmt.Println(ejercicios.TabladeMultiplicar())
+					numero, texto := ejercicios.ConviertoaNumerico("40")
+					fmt.Println(numero)
+					fmt.Println(texto)
+						}
 
-			//files.GrabaTabla()
-			//	files.LeoArchivo()
-			//arreglos_slices.MuestroSlice()
+					fmt.Println(ejercicios.TabladeMultiplicar())
 
-		Pedro := new(modelos.Hombre)
-		e.HumanosRespirando(Pedro)
+				//files.GrabaTabla()
+				//	files.LeoArchivo()
+				//arreglos_slices.MuestroSlice()
 
-		Maria := new(modelos.Mujer)
-		e.HumanosRespirando(Maria)
-	defer_panic.VemosDefer()*/
-	canal1 := make(chan bool)
-	go gorutines.MiNombreLentooo("Anny DC", canal1)
+			Pedro := new(modelos.Hombre)
+			e.HumanosRespirando(Pedro)
 
-	defer func() {
-		<-canal1
+			Maria := new(modelos.Mujer)
+			e.HumanosRespirando(Maria)
+		defer_panic.VemosDefer()
+		canal1 := make(chan bool)
+		go gorutines.MiNombreLentooo("Anny DC", canal1)
 
-	}()
-	fmt.Println("Estoy aqui")
+		defer func() {
+			<-canal1
+
+		}()
+		fmt.Println("Estoy aqui")
+	webserver.MiWebServer()*/
+	middleware.MiMidlleware()
 
 }
